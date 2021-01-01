cc7319b14fecca714cbd204969449d8c915abfe6
abhisekganguly <meaw221bbaker@gmail.com>
2021-01-01 21:58:50 +0530

    core-components

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TextInput,
+  Platform,
+} from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <ScrollView>
+        <Text>First line of Text.</Text>
+        <View>
+          <Text>Second line of text.</Text>
+          <Image
+            source={{ uri: "https://reactnative.dev/docs/assets/p_cat2.png" }}
+            style={{ width: 200, height: 200 }}
+          />
+        </View>
+        <TextInput
+          style={{ height: 40, borderColor: "aqua", borderWidth: 1 }}
+          placeholder="Fill me Nigga!"
+        />
+      </ScrollView>
+    </SafeAreaProvider>
   );
-}
+};
+
+console.log("statusBarHeight: ", StatusBar.currentHeight);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    ...Platform.select({
+      ios: {
+        paddingTop: StatusBar.currentHeight,
+      },
+      android: {
+        paddingTop: StatusBar.currentHeight,
+        backgroundColor: "aqua",
+      },
+      default: {
+        paddingTop: 30,
+        backgroundColor: "orange",
+      },
+    }),
   },
 });
+
+export default App;
